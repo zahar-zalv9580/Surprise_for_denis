64b389e46044afa7038d7c3c6126b77e84058283
zahar-zalv9580 <166313024+zahar-zalv9580@users.noreply.github.com>
2025-04-01 10:00:17 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let location = window.top.location // для роботи цього коду на платформі 
-
-let btn = document.querySelector('.redirect-btn')
-btn.addEventListener('click', function() {
-    location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-})
