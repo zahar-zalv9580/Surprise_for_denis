88cdf353c5df5ff3a97091a27e094fe9d01ebb76
zahar-zalv9580 <166313024+zahar-zalv9580@users.noreply.github.com>
2025-04-01 09:41:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+let location = window.top.location // для роботи цього коду на платформі 
 
-body {
-    margin: 0;
-    padding: 0;
-    width: 100vw; height: 100vh;
-    font-family: 'Montserrat', sans-serif; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.redirect-btn {
-    background: #3C4D6C;
-    color: white;
-    padding: 1em 2em;
-    user-select: none;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
-.redirect-btn:hover {
-    background: #495d82;
-    box-shadow: 0 0 5px .5px gray;
-}
-
-.redirect-btn:active {
-    background: #3C4D6C;
-    box-shadow: none;
-}
+let btn = document.querySelector('.redirect-btn')
+btn.addEventListener('click', function() {
+    location.assign('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+})
